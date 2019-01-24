62c86712c31f9441e42707ae4e203209ec810f66
Bailie Wu <wubailie@gmail.com>
2019-01-24 00:54:07 -0500

    first commit all squares

---
+++ stuff.js
@@ -0,0 +1,46 @@
+console.clear()
+const colors = ['yellow', 'orange', 'purple', 'teal'];
+generateDom(colors);//should generate squares, buttons and event handlers
+
+const showButton = document.getElementById('show-btn')
+showButton.addEventListener('click', function(){
+    const squares = document.querySelectorAll('.square')
+    squares.forEach(square => {
+        square.style.display = ''
+    });
+})
+
+const hideButton = document.getElementById('hide-btn')
+hideButton.addEventListener('click', function(){
+    const squares = document.querySelectorAll('.square')
+    squares.forEach(square => {
+        square.style.display = 'none'
+    });
+})
+
+const ul = document.querySelector('ul')
+const squares = document.querySelectorAll('.square')
+squares.forEach( square => {
+  square.addEventListener('click', function(){
+    const li = document.createElement('li');
+    li.innerHTML = `you clicked on ${ this.id}`;
+    ul.appendChild(li);
+  })
+});
+
+function generateDom(colorArr) {
+    // add a class of square in squarecontainer class
+    const squareContainer = document.querySelector('.squareContainer')
+    // console.log(squareContainer.children)
+    // let square = document.createElement('div')
+    // square.className = 'square'
+    // square.style.backgroundColor = 'red'
+    // squareContainer.appendChild(square)
+    colorArr.forEach(color => {
+    let square = document.createElement('div')
+    square.className = 'square'
+    square.style.backgroundColor = color
+    squareContainer.appendChild(square)
+    });
+
+}
